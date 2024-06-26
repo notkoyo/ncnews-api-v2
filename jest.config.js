b1a99bfda0fbb8b71ab68a6f5d5be6b8360c99cb
notkoyo <135956230+notkoyo@users.noreply.github.com>
2024-06-26 12:45:48 +0100

    update jest config

--- jest.config.js
+++ jest.config.js
@@ -2,16 +2,6 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-  testMatch: ['**/?(*.)+(spec|test).+(ts|tsx|js)'],
-  transform: {
-    '^.+\\.tsx?$': ['ts-jest'],
-  },
-};
-
-module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   transform: {
     '^.+\\.tsx?$': ['ts-jest'],
   },
